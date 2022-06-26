835b62c114ef9c6895c4051c2d1c194375a28c60
TOtooRo <35839248+xeonu@users.noreply.github.com>
2022-06-26 22:52:46 +0900

    Initial commit

---
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -0,0 +1,29 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController(UserDaoService service){
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllusers(){
+        return service.findAll();
+    }
+
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return service.findOne(id);
+    }
+
+
+}
